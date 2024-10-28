5b3b0471c185f91877a1d93b8ed926e3c75b1fcc
Francesco Botto s331646 <s331646@studenti.polito.it>
2024-10-28 14:07:00 -0500

    Separate services

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://chatbot-ui-xvnb.onrender.com"],  # Allows requests from your React app's port and Render
+    allow_origins=["http://localhost:5173", "https://chatbot-ui-xvnb.onrender.com"],  # http://localhost:5173 for local use
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Allows all headers
